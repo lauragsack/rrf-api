f59ad6eef70a4f8c5c48e58be39135563d8045ab
lsack <lauragsack@gmail.com>
2020-05-20 14:01:12 -0700

    updates frontend origin on server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const routes = require("./routes");
 
 // Handle Cors
 const corsOptions = {
-    origin: ["http://localhost:3000", "https://stormy-eyrie-04598.herokuapp.com/"],
+    origin: ["http://localhost:3000", "https://russian-river-floaties.herokuapp.com/"],
     credentials: true,
     optionsSuccessStatus: 200
 }
